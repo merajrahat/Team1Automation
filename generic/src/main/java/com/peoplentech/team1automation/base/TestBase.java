25b0b1cf1e98944b686c55ca73c13f36ab4102fb
nafaikermoud <duomreki@gmail.com>
2020-11-22 21:25:19 -0500

    first

--- generic/src/main/java/com/peoplentech/team1automation/base/TestBase.java
+++ generic/src/main/java/com/peoplentech/team1automation/base/TestBase.java
@@ -18,6 +18,8 @@ import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 
+// it works
+
 import java.io.File;
 import java.io.PrintWriter;
 import java.io.StringWriter;
