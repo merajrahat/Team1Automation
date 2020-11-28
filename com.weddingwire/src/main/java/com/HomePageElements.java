b9206b3aea0b01e74a48e3408b279df1acdba585
ipipduh <philippakramou@gmail.com>
2020-11-27 20:34:21 -0500

    adding changes

---
+++ com.weddingwire/src/main/java/com/HomePageElements.java
@@ -0,0 +1,121 @@
+package com;
+
+public class HomePageElements {
+
+     // click join button
+     public static final String clickOnJoinButtonLocator = "//a[contains(text(),'Join now')]";
+     // enter valid name
+     public static final String sendValidNameLocator = "//input[@name='NombreCompleto']";
+     // enter valid email
+     public static final String sendValidEmailLocator = "//body/div[2]/div[1]/div[2]/div[2]/form[1]/div[1]/div[2]/div[1]/div[1]/input[1]"; // had no choice here, bruh
+     // enter valid password
+     public static final String sendValidPasswordLocator = "//input[@name='Password']";
+     // click on venue and send text
+     public static final String sendValidVenueLocator = "//input[@id='txtStrPoblacion']";
+     // access dropdown and select
+     public static final String sendValidVenueDropdownLocator = "//li[@data-id-poblacion=\"1488913\"]";
+     // access calendar box
+     public static final String clickWeddingDateBoxLocator = "Fecha";
+     // click on calendar dropdown
+     public static final String clickCalendarLocator = "//input[@name='Fecha']";
+     // select wedding year
+     public static final String clickYearLocator = "(//span[@class='year'])[6]";
+     // select wedding month
+     public static final String clickMonthLocator = "//span[contains(text(),'Jun')]";
+     // select wedding day
+     public static final String clickDayLocator = "(//td[@class='day '])[11]";
+     // click sign up button
+     public static final String clickSignUpButtonLocator = "//button[contains(text(),'Sign up')]";
+     // clear the search bar for setting
+     public static final String clearSearchBarLocator = "//input[@id='txtStrSearch']";
+     // enter waterfront for setting in search bar
+     public static final String enterTextInSearchBarLocator = "//input[@id='txtStrSearch']";
+     // clear the location search bar
+     public static final String clearAgainInSearchBarLocator = "//input[@aria-label='NEAR city or region']";
+     // enter destination in location search bar
+     public static final String enterTextAgainInSearchBarLocator = "//input[@aria-label='NEAR city or region']";
+     // click search button
+     public static final String clickSearchButtonLocator = "//input[@id='mainSearchBtn']";
+     // click the down arrow to see more options
+     public static final String clickDownArrowButtonLocator = "//body/div[7]/div[1]/div[2]/div[2]/div[1]/form[1]/div[1]/div[1]/div[1]/div[1]/i[1]/*[1]"; // no other option
+     // select view more button
+     public static final String clickViewMoreButtonLocator = "(//span[contains(text(),'View More')])[1]";
+     // select Beach Wedding
+     public static final String clickBeachWeddingLocator = "//span[contains(text(),'Beach Weddings')]";
+     // view the image options for directory
+     public static final String clickImageViewLocator = "//li[@class='app-directory-filters-change-mode']";
+     // click login button
+     public static final String loginLocator = "//a[contains(text(),'Log in')]";
+     // enter login email
+     public static final String loginEmailLocator = "//input[@id='Mail']";
+     // enter login password
+     public static final String loginPasswordLocator = "//input[@id='Password']";
+     // click on login button
+     public static final String clickLoginLocator = "//button[contains(text(),'Log in')]";
+     // click on checklist link
+     public static final String clickChecklistLocator = "(//li[@class='pure-u-1-8'])[2]";
+     // click on dropdown to see options
+     public static final String dropdownToolsLocator = "(//li[@class='nav-main-item'])[1]";
+     // select on dropdown to select from tools
+     public static final String dropdownToolsOptionLocator = "(//li[@class='pure-u-1-3'])[2]";
+     // click on header banner link
+     public static final String headerBannerLocator = "//a[contains(text(),'keep moving forward')]";
+     // return navigate to previous page
+     public static final String returnNavigationLocator = "//a[contains(text(),'Start Planning')]";
+     // Scroll to Nondiscrimination
+     public static final String elementScrollLocator = "//a[contains(text(),'Nondiscrimination')]";
+     // find Wedding Dress Gallery on page
+     public static final String findVisibleTextLocator = "//h3[contains(text(),'Wedding Dress Gallery')]";
+     // click organize guest list link
+     public static final String organizeGuestListLinkLocator = "//span[contains(text(),'Organize your Guest List')]";
+     // click add guests link
+     public static final String addGuestsLinkLocator = "//p[contains(text(),'Add your guests')]";
+     // handle guest list popup
+     public static final String closePopupLocator = "//button[contains(text(),'×')]";
+     // click on concierge bot chat
+     public static final String conciergeBotLocator = "//body/div[@id='app-chat-container']/div[@id='app-bot-bot']/div[1]/div[1]/img[1]"; // no other option
+     // minimize the concierge bot chat
+     public static final String minimizeConciergeBotLocator = "//body/div[@id='app-chat-container']/div[@id='app-bot-bot']/div[2]/div[1]/div[3]/span[1]/span[1]"; // no other option
+     // click add photographer button
+     public static final String addPhotographerButtonLocator = "//body/div[6]/div[4]/div[3]/div[1]/figure[1]/span[1]"; // no other option
+     // handle popup for adding photographer
+     public static final String handlePopupButtonLocator = "//body/div[@id='app-common-layer']/div[1]/div[1]/div[1]/button[1]/i[1]/*[1]"; // no other option
+     // search for photographer by name
+     public static final String photographerSearchBarLocator = "//input[@id='app-suggest-vendor-text-input']";
+     // select photographer from search bar
+     public static final String photographerSearchSelectionLocator = "//li[@data-id='772935']";
+     // save photographer vendor
+     public static final String savePhotographerSelectionLocator = "//button[contains(text(),'Save vendor')]";
+     // navigate to favorites
+     public static final String favoriteButtonHeartLocator = "//body/div[@id='menu']/div[1]/div[1]/div[1]/div[2]/div[2]/a[1]/i[1]"; // no other option
+     // view favorite photographer
+     public static final String viewFavoritePhotographerClickLocator = "//span[contains(text(),'Photography')]";
+     // remove favorite photographer
+     public static final String removeFavoritePhotographerClickLocator = "//body/div[5]/div[1]/div[2]/div[2]/div[3]/div[1]/div[1]/figure[1]/i[1]";
+     // select profile dropdown
+     public static final String profileClickLocator = "//body/div[@id='menu']/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]";
+     // click profile link
+     public static final String profileLinkClickLocator = "//body/div[@id='menu']/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/div[2]/div[2]/a[1]";
+     // click x to leave popup
+     public static final String exitProfilePopupLocator = "//button[contains(text(),'×')]";
+     // click on mail envelope
+     public static final String mailEnvelopeLocator = "//body/div[@id='menu']/div[1]/div[1]/div[1]/div[2]/div[2]/a[2]/i[1]/*[1]";
+     // click on profile
+     public static final String profileClickButtonLocator = "//body/div[@id='menu']/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]";
+     // click on settings
+     public static final String settingsClickButtonLocator = "//body/div[@id='menu']/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]/a[1]";
+     // click on user information
+     public static final String userInformationLinkLocator = "//a[contains(text(),'User Information')]";
+     // click on notifications link
+     public static final String notificationLinkLocator = "//a[contains(text(),'Notifications')]";
+     // update preference checkbox
+     public static final String notificationPeferenceCheckboxLocator = "//body/div[5]/div[1]/div[2]/form[1]/div[1]/div[1]/div[2]/div[2]/div[1]/label[1]/input[1]";
+     // click save notification setting button
+     public static final String saveNotificationChangesButtonLocator = "//button[contains(text(),'Save changes')]";
+     // click Shop link
+     public static final String clickShopLinkLocator = "//span[contains(text(),'Shop')]";
+     // sign up, one last step
+     public static final String validateSignUpLocator = "//p[contains(text(),'One last step!')]";
+
+
+}
---
+++ com.weddingwire/src/test/java/com/test/HomePageTestRunner.java
@@ -0,0 +1,233 @@
+package com.test;
+
+import com.WeddingWire_HomePage;
+import com.peoplentech.team1automation.base.TestBase;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class HomePageTestRunner extends TestBase {
+    WeddingWire_HomePage weddingWire_homePage;
+
+    // 1
+    @Test(enabled = true)
+    public void navigateToHomePageValidation() {
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.assertionNavigateToHomePageValidation();
+    }
+
+    // 2
+    @Test(enabled = false)
+    public void joinNowValidation() {
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.joinNowElement();
+        weddingWire_homePage.assertionJinNowElement();
+    }
+
+    // 3 - TEST CASE PASSES BUT THE ASSERTION DOES NOT WORK
+    @Test(enabled = false)
+    public void enterInformationValidation() {
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.sendValidInformation();
+        weddingWire_homePage.assertionValidInformation();
+    }
+
+    // 4
+    @Test(enabled = false)
+    public void searchBarValidation() {
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.accessSearchBar();
+        weddingWire_homePage.assertionAccessSearchBar();
+    }
+
+    // 5
+    @Test(enabled = false) // passes but does not assert?
+    public void venueFiltersValidation() {
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.accessVenueFilters();
+        weddingWire_homePage.assertionAccessVenueFilters();
+    }
+
+    // 6
+    @Test(enabled = false) // FAILS WHY
+    public void resultsViewsValidation() {
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.resultsViewOptions();
+        weddingWire_homePage.assertionResultsViewOptions();
+    }
+
+    // 7
+    @Test(enabled = false) // PASSES BUT DOES NOT ASSERT
+    public void LoginValidation() {
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.userLogin();
+        weddingWire_homePage.assertionUserLogin();
+    }
+
+    // 8
+    @Test(enabled = false)
+    public void checklistValidation() {
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.checklistAccess();
+        weddingWire_homePage.assertionChecklistAccess();
+    }
+
+    // 9
+    @Test(enabled = false) // Sometimes passes sometimes fails always at the Planning Tools part
+    public void dropdownClickValidation() {
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.dropdownClickTools();
+        weddingWire_homePage.assertionDropdownClickTools();
+    }
+
+    // 10
+    @Test(enabled = false) // PASSES BUT ASSERTION FAILS (OPENS INTO A NEW BROWSER)
+    public void headerClickValidation() {
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.headerLinkClick();
+        weddingWire_homePage.assertionHeaderLinkClick();
+    }
+
+    // 11
+    @Test(enabled = false) // PASSES BUT ASSERTION FAILS (OPENS INTO A NEW BROWSER)
+    public void navigateBackValidation() {
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.goBackClick();
+        navigateBack();
+        weddingWire_homePage.assertionGoBackClick();
+    }
+
+    // 12
+    @Test(enabled = false)
+    public void scrollElementValidation() {
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("window.scrollBy(0,1500)");
+        weddingWire_homePage.scrollElementClick();
+        weddingWire_homePage.assertionScrollElementClick();
+    }
+
+    // 13
+    @Test(enabled = false)
+    public void visibleTextValidation() {
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.findingVisibleText();
+        weddingWire_homePage.assertionFindingVisibleText();
+    }
+
+    // 19
+    @Test(enabled = false) // Assertion fails
+    public void guestListClickValidation() {
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.clickGuestListElement();
+        weddingWire_homePage.assertionClickGuestListElement();
+    }
+
+    // 20
+    @Test(enabled = false)
+    public void addGuestListClickValidation() throws InterruptedException {
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.addGuestLinkElement();
+        weddingWire_homePage.assertionAddGuestLinkElement();
+    }
+
+    // 21
+    @Test(enabled = false) // unsure how to assert this?
+    public void conciergeClickValidation() throws InterruptedException {
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.conciergeClockElement();
+        weddingWire_homePage.assertionConciergeClockElement();
+    }
+
+    // 22
+    @Test(enabled = false)
+    public void buttonAndPopUpValidation() throws InterruptedException {
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.buttonAndPopupELement();
+        weddingWire_homePage.assertionButtonAndPopupELement();
+    }
+
+    // 23
+    @Test(enabled = false)
+    public void photographerSelectionValidation() throws InterruptedException {
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.photographerSearchELement();
+        weddingWire_homePage.assertionPhotographerSearchELement();
+    }
+
+    //24
+    @Test(enabled = false)
+    public void favoriteButtonValidation() throws InterruptedException {
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.favoriteButtonELement();
+        weddingWire_homePage.assertionFavoriteButtonELement();
+    }
+
+//    // 25
+//    @Test(enabled = false) // help handling that popup. doesn't work
+//    public void removeFavoriteValidation() throws InterruptedException {
+//        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+//        weddingWire_homePage.removeFavoritePhotographerClick();
+//        String expectedResult = "Pippa | Weddingwire";
+//        String actualResult = driver.getTitle();
+//        Assert.assertEquals(actualResult, expectedResult, "Test Failed Result Not Match");
+//    }
+
+    // 26
+    @Test(enabled = false)
+    public void profileAccessValidation() throws InterruptedException {
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.profileViewELement();
+        weddingWire_homePage.assertionProfileViewELement();
+    }
+
+    // 27
+    @Test(enabled = false)
+    public void mailboxAccessValidation() throws InterruptedException {
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.viewMailboxELement();
+        weddingWire_homePage.assertionViewMailboxELement();
+    }
+
+    // 28
+    @Test(enabled = false)
+    public void settingsAccessValidation() throws InterruptedException {
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.viewSettingsELement();
+        weddingWire_homePage.assertionViewSettingsELement();
+    }
+
+    // 29
+    @Test(enabled = false)
+    public void changeNotificationSettingValidation() throws InterruptedException {
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.viewNotificationELement();
+        String expectedResult = "Weddings, Wedding - WeddingWire.com";
+        String actualResult = driver.getTitle();
+        Assert.assertEquals(actualResult, expectedResult, "Test Failed Result Not Match");
+    }
+
+    // 30
+    @Test(enabled = false)
+    public void accessShopPageValidation() throws InterruptedException {
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        weddingWire_homePage = PageFactory.initElements(driver, WeddingWire_HomePage.class);
+        weddingWire_homePage.clickShopLinkELement();
+        weddingWire_homePage.assertionClickShopLinkELement();
+    }
+
+// add to cart, remove from cart, search for item, dropdown, check for hover options, etc
+    // iframe on regular website
+}
