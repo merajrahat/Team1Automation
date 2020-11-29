40a9050803487016b17c14b25dca84ea3d3368c3
Abir muhaimin <am@Abirs-MacBook-Pro.local>
2020-11-29 13:29:45 -0600

    updatedextent

--- com.weddingwire/src/main/java/weddingwire/pages/HomePage.java
+++ com.weddingwire/src/main/java/weddingwire/pages/HomePage.java
@@ -1,7 +1,8 @@
 package weddingwire.pages;
 
 import com.peoplentech.team1automation.base.TestBase;
-import com.peoplentech.team1automation.report.ExtentTestManager;
+import com.peoplentech.team1automation.extent.ExtentTestManager;
+import com.peoplentech.team1automation.extent.ExtentTestManager;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
